1720c5269b7285302c5c6287f3814dcb8c33fef2
roja2004balraj <149159035+roja2004balraj@users.noreply.github.com>
2023-10-27 03:18:55 -0700

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,6 @@
+javascript
+function startLesson(lessonNumber) {
+    // Replace this with your logic to start and display the selected lesson.
+    alert("Starting Lesson " + lessonNumber);
+    // You can use DOM manipulation to display lesson content and exercises.
+}
